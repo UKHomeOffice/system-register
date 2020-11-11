b5b47f9c8d7dec47868843e959edcc2e4ef9bffc
James Brown <jameskinnahbrown@gmail.com>
2020-11-11 14:55:32 +0000

    Replace TitleBar with govuk-react components

--- frontend/src/components/TitleBar/TitleBar.js
+++ frontend/src/components/TitleBar/TitleBar.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import Auth from '../Auth/Auth' 
+import Auth from '../Auth/Auth'
 import './TitleBar.css'
+import TopNav, { asNavLinkAnchor } from '@govuk-react/top-nav';
+
+const NavAnchor = asNavLinkAnchor('a');
+
 
 const TitleBar = () => {
+
     return (
-        <div data-testid="title-bar-div" className="title bottomMargin">
-                <h2 className="title">System Register</h2>
-           <Auth />
-        </div>
+        <TopNav data-testid="title-bar-div" company="" serviceTitle="System Register" active={0}></TopNav>
+        // <div data-testid="title-bar-div" className="title bottomMargin">
+        //     <h2 className="title">System Register</h2>
+        //     <Auth />
+        // </div>
     )
 }
 
--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
     keycloak: {
-        url: process.env.REACT_APP_KEYCLOAK_URL,
-        realm: process.env.REACT_APP_KEYCLOAK_REALM,
-        clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+        url: process.env.REACT_APP_KEYCLOAK_URL || "unset-url",
+        realm: process.env.REACT_APP_KEYCLOAK_REALM || "unset-realm",
+        clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || "unset-client-id",
     },
     api: {
         url: `${window.location.origin}/api`
