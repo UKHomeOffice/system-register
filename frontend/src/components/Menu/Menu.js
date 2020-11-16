dc966a1415fe5afe7ac6f81b90214f679d1fa75f
James Brown <jameskinnahbrown@gmail.com>
2020-11-16 15:37:55 +0000

    working on conforming our navbar to GDS

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -4,14 +4,14 @@ import './Menu.css'
 
 const Menu = () => {
     return (
-        <div className="Menu">
-            <ul>
+        <nav className="Menu">
+            <ul className="centerContent">
                 <li><NavLink exact to="/" activeClassName="selected">System Register</NavLink></li>
                 <li><NavLink to="/risk_dashboard" activeClassName="selected">Risk Dashboard</NavLink></li>
                 <li><NavLink to="/about" activeClassName="selected">About</NavLink></li>
                 <li><NavLink to="/contact" activeClassName="selected">Contact</NavLink></li>
             </ul>
-        </div>
+        </nav>
     )
 }
 
