f60ec6a474474897fe82e70ba5ba9e497ceab82b
waralenda <paula.siemek@thoughtworks.com>
2020-11-17 14:53:59 +0000

    SR-7 Add API link to nav bar
    JB & JC & PS

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Menu.css'
+import config from "../../config/config";
 
 const Menu = () => {
     return (
@@ -8,6 +9,7 @@ const Menu = () => {
             <ul className="centerContent">
                 <li><NavLink exact to="/" activeClassName="selected">System Register</NavLink></li>
                 <li><NavLink to="/risk_dashboard" activeClassName="selected">Risk Dashboard</NavLink></li>
+                <li><a href={`${config.api.url}/systems`}>API</a></li>
                 <li><NavLink to="/about" activeClassName="selected">About</NavLink></li>
                 <li><NavLink to="/contact" activeClassName="selected">Contact</NavLink></li>
             </ul>
--- frontend/src/components/Menu/__test__/Menu.test.js
+++ frontend/src/components/Menu/__test__/Menu.test.js
@@ -4,6 +4,8 @@ import { createMemoryHistory } from 'history'
 import renderer from 'react-test-renderer'
 import { BrowserRouter, Router, MemoryRouter } from 'react-router-dom'
 import Menu from '../Menu'
+import config from "../../../config/config";
+
 
 describe('<Menu />', () => {
     it('renders System Register link', () => {
@@ -20,14 +22,21 @@ describe('<Menu />', () => {
         expect(element).toHaveAttribute("href", '/risk_dashboard')
     });
 
-    it('renders Risk About link', () => {
+    it('renders API link', () => {
+        const { getByText } = render(<MemoryRouter><Menu /></MemoryRouter>)
+        const element = getByText('API')
+        expect(element).toBeInTheDocument()
+        expect(element).toHaveAttribute("href", `${config.api.url}/systems`)
+    });
+
+    it('renders About link', () => {
         const { getByText } = render(<MemoryRouter><Menu /></MemoryRouter>)
         const element = getByText('About')
         expect(element).toBeInTheDocument()
         expect(element).toHaveAttribute("href", '/about')
     });
 
-    it('renders Risk Contact link', () => {
+    it('renders Contact link', () => {
         const { getByText } = render(<MemoryRouter><Menu /></MemoryRouter>)
         const element = getByText('Contact')
         expect(element).toBeInTheDocument()
