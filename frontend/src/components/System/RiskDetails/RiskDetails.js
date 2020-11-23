41f03bec6f4cfc222f5cfc0d9f1beb5474ed454f
James Brown <jameskinnahbrown@gmail.com>
2020-11-23 11:55:13 +0000

    fix axe issue in <RiskDetail /> to set header correctly

--- frontend/src/components/System/RiskDetails/RiskDetails.js
+++ frontend/src/components/System/RiskDetails/RiskDetails.js
@@ -8,7 +8,7 @@ const NOT_APPLICABLE = 'not_applicable'
 const RiskDetails = (props) => {
   return (
     <div className='riskSummary'>
-      <h4>{toTitle(props.risk.name)}</h4>
+      <h3>{toTitle(props.risk.name)}</h3>
       <RiskBadge level={props.risk.level} />
       {renderRationale(props.risk.level, props.risk.rationale, props.risk.name)}
     </div>
