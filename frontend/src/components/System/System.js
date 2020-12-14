faed3c5a481e09957b3d00fbbcfa5599d460edeb
Mike Winter <mwinter@thoughtworks.com>
2020-12-14 14:52:31 +0000

    SR-67: Map system view UI to default route

--- frontend/src/components/System/System.js
+++ frontend/src/components/System/System.js
@@ -1,20 +1,26 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from "react-router-dom";
-import './System.css'
-import api from '../../services/api';
-import SystemView from "./SystemView/SystemView";
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, useParams } from 'react-router-dom';
 
-const System = () => {
-  let {id} = useParams();
-  let [system, setSystem] = useState(null);
+import SystemView from "./SystemView/SystemView";
+import api from '../../services/api';
+import './System.css';
+
+function System() {
+  const { id } = useParams();
+  const [system, setSystem] = useState(null);
 
   useEffect(() => {
-      const fetchData = async () => setSystem(await api.getSystem(id));
-      fetchData()
-    },
-    [id]);
+    const fetchData = async () => setSystem(await api.getSystem(id));
+    fetchData();
+  }, [id]);
 
-  return (<SystemView system={system}/>)
-};
+  return (
+    <Switch>
+      <Route path="" exact>
+        <SystemView system={system} />
+      </Route>
+    </Switch>
+  );
+}
 
-export default System
+export default System;
--- frontend/src/components/System/__test__/System.test.js
+++ frontend/src/components/System/__test__/System.test.js
@@ -1,41 +1,39 @@
-import React from 'react'
-import {cleanup, render} from '@testing-library/react'
-import {screen} from '@testing-library/dom'
-import System from '../System'
-import {MemoryRouter, Route} from 'react-router-dom'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
 
+import System from '../System';
 import api from '../../../services/api';
 
 jest.mock('../../../services/api', () => ({
   getSystem: jest.fn(),
-}))
+}));
 
 const test_system = {
   name: "Test System",
   last_updated: {},
   risks: [],
-  aliases: []
-}
+  aliases: [],
+};
 
 describe('<System />', () => {
-  afterEach(cleanup)
   beforeEach(() => {
     api.getSystem.mockResolvedValue(test_system);
   });
 
-  it('renders system name', async () => {
+  it('renders system view', async () => {
     setup();
+
     const element = await screen.findByText('Test System');
+
     expect(element).toBeInTheDocument();
   });
-
 });
 
 function setup() {
   render(
     <MemoryRouter initialEntries={['system/1']}>
-      <Route path='system/:id'>
-        <System/>
-      </Route>
-    </MemoryRouter>)
+      <Route path='system/:id' component={System} />
+    </MemoryRouter>
+  );
 }
