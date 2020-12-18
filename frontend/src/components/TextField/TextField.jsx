73b9f3cd2d4ee4e2096740dcc0f980d3cddeabfd
Mike Winter <mwinter@thoughtworks.com>
2020-12-18 15:43:25 +0000

    SR-67: Prefix field errors with hidden "Error:" text

--- frontend/src/components/TextField/TextField.jsx
+++ frontend/src/components/TextField/TextField.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import { Field } from "formik";
 import { InputField } from "govuk-react";
 
+import "./TextField.css";
+
 const emptyIfUndefined = (value) => value != null ? value : "";
 
 function TextField({ children, hint, name, placeholder, validate, inputClassName }) {
   return <Field name={name} validate={validate}>
-    {({ field: { value, ...fieldProps }, meta: { error, touched } }) => {
+    {({ field: { value, ...fieldProps }, meta: { error: errorText, touched } }) => {
       const inputProps = {
         value: emptyIfUndefined(value),
         className: inputClassName,
         placeholder,
         ...fieldProps
       };
+      const error = errorText && (
+        <>
+          <span className="visually-hidden">Error:</span>
+          {errorText}
+        </>
+      );
 
       return (
         <InputField hint={hint} input={inputProps} meta={{ error, touched }}>
