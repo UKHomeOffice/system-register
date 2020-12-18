6fa71d56ff61656a4ddfddf483bda0c58f193a54
Mike Winter <mwinter@thoughtworks.com>
2020-12-18 16:50:33 +0000

    Allow UI config to work in both local and production environments

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,21 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
+
+const baseUrl = process.env.NODE_ENV === "development"
+  ? "http://localhost:8080"
+  : "";
 
 const config = {
+  baseUrl,
   getKeycloakConfig,
   api: {
-    // url: `http://localhost:8080/api`
-    url: `${window.location.origin}/api`
+    url: `${baseUrl}/api`
   },
-}
+};
 
 async function getKeycloakConfig() {
-  // const res = await axios.get('http://localhost:8080/config/keycloak');
-  const res = await axios.get('/config/keycloak');
+  const res = await axios.get(`${baseUrl}/config/keycloak`);
   return {
     url: res.data.host,
     realm: res.data.realm,
-    clientId: res.data.clientId
-  }
+    clientId: res.data.clientId,
+  };
 }
 
-export default config
+export default config;
