db146a2b85ab937a0c43e0d79b9d247902f94bde
paulasiemek <paula.siemek@thoughtworks.com>
2020-12-22 11:22:25 +0100

    SR-158 Fix colour issues in chrome, make footer stick to the bottom of page
    Paula

--- frontend/src/components/SRFooter/SRFooter.js
+++ frontend/src/components/SRFooter/SRFooter.js
@@ -1,36 +1,32 @@
 import React from "react";
 import "./SRFooter.css"
-import crest from "./govuk-crest.png"
 import ogl from "./ogl-symbol.png"
-import {GridCol, GridRow} from 'govuk-react';
 
 
 const SRFooter = () => {
   return (
     <div className="SRFooter">
       <div className="centerContent">
-        <GridRow>
-          <GridCol setWidth="85%">
+        <div className="meta-flex">
+          <div className="meta-item">
             <ul className="footer-list">
               <li><a className="link" href="/about">About</a></li>
               <li><a className="link" href="/contact">Contact</a></li>
               <li><a className="link" href="/api/systems">API</a></li>
             </ul>
-            <span >
+            <div>
               <img className="ogl" src={ogl} alt="OGL"/>
-                <span>All content is available under the <a className="link"
-                                                            href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated </span>
-            </span>
-          </GridCol>
-          <GridCol setWidth="15%">
-            <div className="copyright">
-              <a
-                href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/">
-                <img className="crest" src={crest} alt="Crown copyright"/>
-              </a>
+              <span>All content is available under the <a className="link"
+                                                          href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated </span>
             </div>
-          </GridCol>
-        </GridRow>
+          </div>
+          <div className="copyright">
+            <a className="crest link"
+               href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/">
+              © Crown copyright
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -2,86 +2,90 @@ import React from 'react'
 import './App.css';
 import SystemList from '../components/SystemList/SystemList'
 import TitleBar from '../components/TitleBar/TitleBar'
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 import System from '../components/System/System'
 import Menu from '../components/Menu/Menu'
 import PortfolioHeatmap from '../components/Visualisations/PortfolioHeatmap/PortfolioHeatmap';
-import { KeycloakProvider } from '@react-keycloak/web'
+import {KeycloakProvider} from '@react-keycloak/web'
 import Banner from '../components/Banner/Banner'
 import api from '../services/api';
 import config from '../config/config';
 import Keycloak from 'keycloak-js'
+import SRFooter from "../components/SRFooter/SRFooter";
 
 // todo state should be in containers, components should be stateless
 class App extends React.Component {
-    state = {
-        keycloak: undefined,
-        register: {
-            "systems": []
-        }
+  state = {
+    keycloak: undefined,
+    register: {
+      "systems": []
     }
+  }
 
-    componentDidMount() {
-        this._isMounted = true;
-        config.getKeycloakConfig()
-            .then((c) => {
-                const keycloakConfig = {
-                    url: c.url,
-                    realm: c.realm,
-                    clientId: c.clientId
-                }
-                this.setState(
-                    {
-                        keycloak: new Keycloak(keycloakConfig),
-                        register: this.state.register
-                    })
-            }).catch((e) => console.error(e))
+  componentDidMount() {
+    this._isMounted = true;
+    config.getKeycloakConfig()
+    .then((c) => {
+      const keycloakConfig = {
+        url: c.url,
+        realm: c.realm,
+        clientId: c.clientId
+      }
+      this.setState(
+        {
+          keycloak: new Keycloak(keycloakConfig),
+          register: this.state.register
+        })
+    }).catch((e) => console.error(e))
 
-        api.getAllSystems()
-            .then((register) => {
-                if (this._isMounted) {
-                    this.setState(
-                        {
-                            keycloak: this.state.keycloak,
-                            register: register
-                        })
-                }
-            })
+    api.getAllSystems()
+    .then((register) => {
+      if (this._isMounted) {
+        this.setState(
+          {
+            keycloak: this.state.keycloak,
+            register: register
+          })
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    if (this.state.keycloak) {
+      return (
+        <KeycloakProvider keycloak={this.state.keycloak}>
+          <BrowserRouter>
+            <header>
+              <TitleBar/>
+              <Banner phase="in development">
+                This is a new service - your <Link className="gds-link" to="/contact">feedback</Link> will help us to
+                improve it.
+              </Banner>
+            </header>
+            <div className="page">
+            <Menu/>
+            <main className="content-wrap">
+              <Switch>
+                <Route exact path="/">
+                  <SystemList register={this.state.register}/>
+                </Route>
+                <Route path="/system/:id" component={System}/>
+                <Route exact path="/risk_dashboard" component={PortfolioHeatmap}/>
+              </Switch>
+            </main>
+            <SRFooter/>
+            </div>
+          </BrowserRouter>
+        </KeycloakProvider>
+      );
+    } else {
+      return <main><p data-testid="auth-initialising-msg">Initialising authentication...</p></main>
     }
-
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
-
-    render() {
-        if (this.state.keycloak) {
-            return (
-                <KeycloakProvider keycloak={this.state.keycloak}>
-                    <BrowserRouter>
-                        <header>
-                            <TitleBar />
-                            <Banner phase="in development">
-                                This is a new service - your <Link className="gds-link" to="/contact">feedback</Link> will help us to improve it.
-                    </Banner>
-                        </header>
-                        <Menu />
-                        <main>
-                            <Switch>
-                                <Route exact path="/">
-                                    <SystemList register={this.state.register} />
-                                </Route>
-                                <Route path="/system/:id" component={System} />
-                                <Route exact path="/risk_dashboard" component={PortfolioHeatmap} />
-                            </Switch>
-                        </main>
-                    </BrowserRouter>
-                </KeycloakProvider>
-            );
-        }
-        else {
-            return <main><p data-testid="auth-initialising-msg">Initialising authentication...</p></main>
-        }
-    }
+  }
 }
 
 export default App;
