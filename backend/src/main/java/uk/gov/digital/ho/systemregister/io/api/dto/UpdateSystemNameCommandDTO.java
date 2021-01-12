5144b518752d9f766ef98d7ea478a3a06a54337d
Jade Corcoran <jade.corcoran@thoughtworks.com>
2021-01-12 12:13:03 +0000

    tech debt cleanup - removed uneccessary jsonProperty annotation

--- backend/src/main/java/uk/gov/digital/ho/systemregister/io/api/dto/UpdateSystemNameCommandDTO.java
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/io/api/dto/UpdateSystemNameCommandDTO.java
@@ -1,8 +1,5 @@
 package uk.gov.digital.ho.systemregister.io.api.dto;
 
-import javax.json.bind.annotation.JsonbProperty;
-
 public class UpdateSystemNameCommandDTO {
-    @JsonbProperty("name")
     public String name;
 }
