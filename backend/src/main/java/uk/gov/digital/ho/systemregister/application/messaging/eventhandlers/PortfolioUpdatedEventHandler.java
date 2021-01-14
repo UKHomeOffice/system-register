9a5b831a0c77cbd68d4f4fbba730d730038b2f79
Mike Winter <mwinter@thoughtworks.com>
2021-01-14 11:44:07 +0000

    SR-24: Add portfolio updated event handler

---
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/eventhandlers/PortfolioUpdatedEventHandler.java
@@ -0,0 +1,19 @@
+package uk.gov.digital.ho.systemregister.application.messaging.eventhandlers;
+
+import uk.gov.digital.ho.systemregister.application.messaging.events.SR_SystemEvent;
+import uk.gov.digital.ho.systemregister.io.database.IEventStore;
+
+import javax.inject.Named;
+
+public class PortfolioUpdatedEventHandler implements EventHandler {
+    private final IEventStore eventStore;
+
+    public PortfolioUpdatedEventHandler(@Named("postgres") IEventStore eventStore) {
+        this.eventStore = eventStore;
+    }
+
+    @Override
+    public void handle(SR_SystemEvent event) {
+        eventStore.save(event);
+    }
+}
