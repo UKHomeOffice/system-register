21958de5b9d0cba03f18677b732763b5533b4363
Mike Winter <mwinter@thoughtworks.com>
2021-01-14 11:39:09 +0000

    SR-24: Add update portfolio command

---
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/commands/UpdatePortfolioCommand.java
@@ -0,0 +1,61 @@
+package uk.gov.digital.ho.systemregister.application.messaging.commands;
+
+import com.google.common.base.Objects;
+import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.CommandHasNoEffectException;
+import uk.gov.digital.ho.systemregister.application.messaging.events.PortfolioUpdatedEvent;
+import uk.gov.digital.ho.systemregister.application.messaging.events.SystemDescriptionUpdatedEvent;
+import uk.gov.digital.ho.systemregister.domain.SR_Person;
+import uk.gov.digital.ho.systemregister.domain.SR_System;
+
+import java.time.Instant;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UpdatePortfolioCommand implements Command {
+    private final int id;
+    @Size(min = 2, message = "You must enter a portfolio.")
+    @NotNull
+    private final String portfolio;
+    @NotNull
+    private final SR_Person author;
+    @NotNull
+    private final Instant timestamp;
+
+    @SuppressWarnings("CdiInjectionPointsInspection")
+    public UpdatePortfolioCommand(int id, String portfolio, SR_Person author, Instant timestamp) {
+        this.id = id;
+        this.portfolio = portfolio == null ? null : portfolio.trim();
+        this.author = author;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public SR_Person getAuthor() {
+        return author;
+    }
+
+    @Override
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public PortfolioUpdatedEvent toEvent() {
+        return new PortfolioUpdatedEvent(id, portfolio, author, timestamp);
+    }
+
+    @Override
+    public void ensureCommandUpdatesSystem(SR_System system) throws CommandHasNoEffectException {
+        if (!willUpdate(system)) {
+            throw new CommandHasNoEffectException("portfolio is the same: " + portfolio);
+        }
+    }
+
+    public boolean willUpdate(SR_System system) {
+        return !Objects.equal(portfolio, system.portfolio);
+    }
+}
