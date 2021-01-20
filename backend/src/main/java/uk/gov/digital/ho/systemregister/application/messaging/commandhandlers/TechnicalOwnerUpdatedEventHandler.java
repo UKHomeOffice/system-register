6c35cf584da6d260f75ee850ba35c386e4ca5872
Mike Winter <mwinter@thoughtworks.com>
2021-01-20 16:39:28 +0000

    SR-267: Add handler for technical owner updated event

--- backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/commandhandlers/TechnicalOwnerUpdatedEventHandler.java
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/commandhandlers/TechnicalOwnerUpdatedEventHandler.java
@@ -2,10 +2,19 @@ package uk.gov.digital.ho.systemregister.application.messaging.commandhandlers;
 
 import uk.gov.digital.ho.systemregister.application.messaging.eventhandlers.EventHandler;
 import uk.gov.digital.ho.systemregister.application.messaging.events.SR_SystemEvent;
+import uk.gov.digital.ho.systemregister.io.database.IEventStore;
+
+import javax.inject.Named;
 
 public class TechnicalOwnerUpdatedEventHandler implements EventHandler {
+    private final IEventStore eventStore;
+
+    public TechnicalOwnerUpdatedEventHandler(@Named("postgres") IEventStore eventStore) {
+        this.eventStore = eventStore;
+    }
+
     @Override
     public void handle(SR_SystemEvent event) {
-        throw new UnsupportedOperationException();
+        eventStore.save(event);
     }
 }
