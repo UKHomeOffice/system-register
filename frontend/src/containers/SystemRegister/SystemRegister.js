7e5369d2e8719bbb4e090c8180cd828a82c75b98
Mike Winter <mwinter@thoughtworks.com>
2021-01-21 10:39:41 +0000

    SR-267: Formatting

--- frontend/src/containers/SystemRegister/SystemRegister.js
+++ frontend/src/containers/SystemRegister/SystemRegister.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import { ErrorBoundary } from 'react-error-boundary';
 
 import About from '../../components/About';
@@ -18,74 +17,79 @@ import PageNotFoundError from '../../components/Errors/PageNotFoundError';
 import getPortfolios from '../getPortfolios';
 import api from '../../services/api';
 
-
 class SystemRegister extends React.Component {
-    state = {
-        register: {
-            "systems": []
-        }
+  state = {
+    register: {
+      "systems": []
     }
+  }
 
-    loadSystems = async () => api.getAllSystems()
-        .then((register) => {
-            if (this._isMounted) {
-                this.setState(
-                    {
-                        register: register
-                    });
-            }
-        });
-
-    componentDidMount() {
-        this._isMounted = true;
+  loadSystems = async () => api.getAllSystems()
+    .then((register) => {
+      if (this._isMounted) {
         this.setState(
-            {
-                register: this.state.register
-            })
+          {
+            register: register
+          });
+      }
+    });
 
-        this.loadSystems()
-    }
+  componentDidMount() {
+    this._isMounted = true;
+    this.setState(
+      {
+        register: this.state.register
+      });
 
-    checkForDuplicateNames = (name) => {
-        name = name.toLowerCase();
-        const allSystemNames = this.state.register.systems.map(system => system.name.toLowerCase())
-        return allSystemNames.includes(name);
-    }
+    this.loadSystems();
+  }
 
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
+  checkForDuplicateNames = (name) => {
+    name = name.toLowerCase();
+    const allSystemNames = this.state.register.systems.map(system => system.name.toLowerCase());
+    return allSystemNames.includes(name);
+  }
 
-    render() {
-        return (<>
-            <header>
-                <TitleBar />
-                <Banner phase="in development">
-                    This is a new service - your <Link to="/contact">feedback</Link> will help us to improve it. </Banner>
-            </header>
-            <div className="page">
-                <Menu />
-                <main className="content-wrap">
-                    <Switch>
-                        <Route exact path="/">
-                            <SystemList register={this.state.register} />
-                        </Route>
-                        <Route path="/system/:id">
-                            <ErrorBoundary FallbackComponent={PageNotFoundError}>
-                                <System portfolios={getPortfolios(this.state.register.systems)} onBeforeNameChange={this.checkForDuplicateNames} onChange={this.loadSystems} />
-                            </ErrorBoundary>
-                        </Route>
-                        <Route exact path="/risk_dashboard" component={PortfolioHeatmap} />
-                        <Route exact path="/about" component={About} />
-                        <Route exact path="/contact" component={Contact} />
-                        <Route path="/*" component={PageNotFoundError} />
-                    </Switch>
-                </main>
-                <SRFooter />
-            </div>
-        </>
-        )
-    }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <TitleBar/>
+          <Banner phase="in development">
+            This is a new service - your <Link to="/contact">feedback</Link> will help us to improve it.
+          </Banner>
+        </header>
+        <div className="page">
+          <Menu/>
+          <main className="content-wrap">
+            <Switch>
+              <Route exact path="/">
+                <SystemList register={this.state.register}/>
+              </Route>
+              <Route path="/system/:id">
+                <ErrorBoundary FallbackComponent={PageNotFoundError}>
+                  <System
+                    portfolios={getPortfolios(this.state.register.systems)}
+                    onBeforeNameChange={this.checkForDuplicateNames}
+                    onChange={this.loadSystems}
+                  />
+                </ErrorBoundary>
+              </Route>
+              <Route exact path="/risk_dashboard" component={PortfolioHeatmap}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/contact" component={Contact}/>
+              <Route path="/*" component={PageNotFoundError}/>
+            </Switch>
+          </main>
+          <SRFooter/>
+        </div>
+      </>
+    );
+  }
 }
 
 export default SystemRegister;
