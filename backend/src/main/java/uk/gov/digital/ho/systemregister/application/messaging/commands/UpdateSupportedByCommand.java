ec2aeec3a9a02993fef39c31e15e6c29785e52a6
Mike Winter <mwinter@thoughtworks.com>
2021-01-26 12:47:44 +0000

    SR-264: Add supported by command

---
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/commands/UpdateSupportedByCommand.java
@@ -0,0 +1,59 @@
+package uk.gov.digital.ho.systemregister.application.messaging.commands;
+
+import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.CommandHasNoEffectException;
+import uk.gov.digital.ho.systemregister.application.messaging.events.SR_SystemEvent;
+import uk.gov.digital.ho.systemregister.domain.SR_Person;
+import uk.gov.digital.ho.systemregister.domain.SR_System;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.util.Objects;
+
+public class UpdateSupportedByCommand implements Command {
+    private final int id;
+    @Pattern(regexp = "^[^!£$%^*<>|~\"=]*$", message = "You must not use the following special characters: ! £ $ % ^ * | < > ~ \" =")
+    @Size(min = 2, message = "Please enter a full name or leave blank if you do not know it.")
+    private final String supportedBy;
+    private final SR_Person author;
+    private final Instant timestamp;
+
+    @SuppressWarnings("CdiInjectionPointsInspection")
+    public UpdateSupportedByCommand(int id, String supportedBy, SR_Person author, Instant timestamp) {
+        this.id = id;
+        this.supportedBy = supportedBy != null ? supportedBy.trim() : null;
+        this.author = author;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public SR_Person getAuthor() {
+        return author;
+    }
+
+    @Override
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public SR_SystemEvent toEvent() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void ensureCommandUpdatesSystem(SR_System system) throws CommandHasNoEffectException {
+        if (!willUpdate(system)) {
+            throw new CommandHasNoEffectException("supporter is the same: " + supportedBy);
+        }
+    }
+
+    public boolean willUpdate(SR_System system) {
+        return !Objects.equals(supportedBy, system.supportedBy);
+    }
+}
---
+++ backend/src/test/java/uk/gov/digital/ho/systemregister/application/messaging/commands/UpdateSupportedByCommandTest.java
@@ -0,0 +1,94 @@
+package uk.gov.digital.ho.systemregister.application.messaging.commands;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.CommandHasNoEffectException;
+import uk.gov.digital.ho.systemregister.domain.SR_Person;
+import uk.gov.digital.ho.systemregister.domain.SR_System;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static uk.gov.digital.ho.systemregister.domain.SR_PersonBuilder.aPerson;
+import static uk.gov.digital.ho.systemregister.helpers.builders.SR_SystemBuilder.aSystem;
+
+class UpdateSupportedByCommandTest {
+    private static final int ID = 2;
+    private static final SR_Person AUTHOR = aPerson()
+            .withUsername("username")
+            .withFirstName("forename")
+            .withSurname("surname")
+            .withEmail("mail@example.com")
+            .build();
+    private static final Instant TIMESTAMP = Instant.now();
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", " "})
+    @EmptySource
+    void supporterMustNotBeTooShort(String supportedBy) {
+        var command = new UpdateSupportedByCommand(ID, supportedBy, AUTHOR, TIMESTAMP);
+
+        var constraintViolations = validator.validate(command);
+
+        assertThat(constraintViolations).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" x", "x "})
+    void extraneousSpacesDoNotCountTowardsTheMinimumCharacterCount(String supportedBy) {
+        var command = new UpdateSupportedByCommand(ID, supportedBy, AUTHOR, TIMESTAMP);
+
+        var constraintViolations = validator.validate(command);
+
+        assertThat(constraintViolations).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"!", "£", "$", "%", "^", "*", "|", "<", ">", "~", "\"", "="})
+    void rejectsStringsContainingInvalidSpecialCharacters(String illegalCharacter) {
+        String supportedBy = "name" + illegalCharacter;
+        var command = new UpdateSupportedByCommand(ID, supportedBy, AUTHOR, TIMESTAMP);
+
+        var constraintViolations = validator.validate(command);
+
+        assertThat(constraintViolations).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"xy", "?-", "Name of person"})
+    @NullSource
+    void allowsSupportedByStringToBeNullOrContainTwoOrMoreCharacters(String supportedBy) {
+        var command = new UpdateSupportedByCommand(ID, supportedBy, AUTHOR, TIMESTAMP);
+
+        var constraintViolations = validator.validate(command);
+
+        assertThat(constraintViolations).isEmpty();
+    }
+
+    @Test
+    void raisesExceptionIfSupportedByValueIsUnchanged() {
+        SR_System system = aSystem()
+                .withId(567)
+                .withSupportedBy("supporter")
+                .build();
+        var command = new UpdateSupportedByCommand(567, "supporter", aPerson().build(), Instant.now());
+
+        assertThatThrownBy(() -> command.ensureCommandUpdatesSystem(system))
+                .isInstanceOf(CommandHasNoEffectException.class)
+                .hasMessageContaining("supporter is the same: supporter");
+    }
+}
