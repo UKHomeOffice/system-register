25ffa6ff6ba968c3aa2fc88cf5ff5fc91a302b1e
Jade Corcoran <jade.corcoran@thoughtworks.com>
2021-01-29 16:03:57 +0000

    [SR-10] added exception mapper

---
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/io/api/DuplicateAliasProvidedExceptionMapper.java
@@ -0,0 +1,20 @@
+package uk.gov.digital.ho.systemregister.io.api;
+
+import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.DuplicateAliasProvidedException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.Map;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+
+@Provider
+public class DuplicateAliasProvidedExceptionMapper implements ExceptionMapper<DuplicateAliasProvidedException> {
+
+    @Override
+    public Response toResponse(DuplicateAliasProvidedException exception) {
+        return Response.status(BAD_REQUEST)
+                .entity(Map.of("errors", Map.of("alias", exception.getMessage()))).build();
+    }
+}
---
+++ backend/src/test/java/uk/gov/digital/ho/systemregister/io/api/DuplicateAliasProvidedExceptionMapperTest.java
@@ -0,0 +1,37 @@
+package uk.gov.digital.ho.systemregister.io.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.DuplicateAliasProvidedException;
+
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.Set;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.map;
+
+class DuplicateAliasProvidedExceptionMapperTest {
+    private DuplicateAliasProvidedExceptionMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new DuplicateAliasProvidedExceptionMapper();
+    }
+
+    @Test
+    void mapsErrors() {
+        DuplicateAliasProvidedException exception = new DuplicateAliasProvidedException(Set.of("duplicate alias"));
+
+        Response response = mapper.toResponse(exception);
+
+        assertThat(response)
+                .extracting(Response::getStatusInfo)
+                .isEqualTo(BAD_REQUEST);
+        assertThat(response.readEntity(new GenericType<Map<String, Object>>() {}))
+                .extracting("errors", map(String.class, String.class))
+                .containsEntry("aliases", exception.getMessage());
+    }
+}
