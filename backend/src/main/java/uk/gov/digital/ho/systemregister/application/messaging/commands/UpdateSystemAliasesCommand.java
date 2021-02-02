93998fae8879b17d21aeb98a1a2f119739eeef6c
Mike Winter <mwinter@thoughtworks.com>
2021-02-02 11:06:37 +0000

    Tech: Remove unused import

--- backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/commands/UpdateSystemAliasesCommand.java
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/application/messaging/commands/UpdateSystemAliasesCommand.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
