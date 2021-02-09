7e6a85335d6d26a747cad560c7c0d0c15f0779ca
Mike Winter <mwinter@thoughtworks.com>
2021-02-09 17:40:09 +0000

    SR-219: Do not rely on default encoding

--- backend/src/main/java/uk/gov/digital/ho/systemregister/util/AES.java
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/util/AES.java
@@ -1,21 +1,16 @@
 package uk.gov.digital.ho.systemregister.util;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.AlgorithmParameterSpec;
-import java.util.Arrays;
-import java.util.Base64;
+import org.jboss.logging.Logger;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
 
-import org.jboss.logging.Logger;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class AES {
     private static final Logger LOG = Logger.getLogger(AES.class);
@@ -24,14 +19,14 @@ public class AES {
 
     public static byte[] encrypt(String plaintext, String key) throws EncryptionError {
         try {
-            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            SecretKey secretKey = new SecretKeySpec(key.getBytes(UTF_8), "AES");
             byte[] iv = new byte[GCM_IV_LENGTH];
             secureRandom.nextBytes(iv);
             final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
             GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
 
-            byte[] cipherText = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
+            byte[] cipherText = cipher.doFinal(plaintext.getBytes(UTF_8));
 
             ByteBuffer byteBuffer = ByteBuffer.allocate(iv.length + cipherText.length);
             byteBuffer.put(iv);
@@ -45,14 +40,14 @@ public class AES {
 
     public static String decrypt(byte[] cipherMessage, String key) throws EncryptionError {
         try {
-            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            SecretKey secretKey = new SecretKeySpec(key.getBytes(UTF_8), "AES");
             final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
             AlgorithmParameterSpec gcmIv = new GCMParameterSpec(128, cipherMessage, 0, GCM_IV_LENGTH);
             cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmIv);
 
             byte[] plainText = cipher.doFinal(cipherMessage, GCM_IV_LENGTH, cipherMessage.length - GCM_IV_LENGTH);
 
-            return new String(plainText, StandardCharsets.UTF_8);
+            return new String(plainText, UTF_8);
         } catch (Exception e) {
             LOG.error("Error encrypting data: " + e.getMessage());
             throw new EncryptionError();
