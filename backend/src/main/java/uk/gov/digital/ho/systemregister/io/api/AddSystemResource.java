aa0e38177057cc9cc1dd72f4680c87ee55e5b061
Mike Winter <mwinter@thoughtworks.com>
2021-02-23 16:10:21 +0000

    SR-319: Use author mapper to extract user information from add system API request

--- backend/src/main/java/uk/gov/digital/ho/systemregister/io/api/AddSystemResource.java
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/io/api/AddSystemResource.java
@@ -1,14 +1,12 @@
 package uk.gov.digital.ho.systemregister.io.api;
 
 import io.quarkus.security.Authenticated;
-import org.eclipse.microprofile.jwt.JsonWebToken;
 import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.AddSystemCommandHandler;
 import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.CommandProcessingException;
 import uk.gov.digital.ho.systemregister.application.messaging.commandhandlers.NoSuchSystemException;
 import uk.gov.digital.ho.systemregister.application.messaging.commands.AddSystemCommand;
 import uk.gov.digital.ho.systemregister.domain.SR_Person;
 import uk.gov.digital.ho.systemregister.io.api.dto.AddSystemCommandDTO;
-import uk.gov.digital.ho.systemregister.io.api.dto.DtoMapper;
 import uk.gov.digital.ho.systemregister.io.api.dto.UpdatedSystemDTO;
 
 import javax.ws.rs.Consumes;
@@ -24,9 +22,11 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("api/systems")
 public class AddSystemResource {
     private final AddSystemCommandHandler handler;
+    private final AuthorMapper authorMapper;
 
-    public AddSystemResource(AddSystemCommandHandler handler) {
+    public AddSystemResource(AddSystemCommandHandler handler, AuthorMapper authorMapper) {
         this.handler = handler;
+        this.authorMapper = authorMapper;
     }
 
     @POST
@@ -37,16 +37,11 @@ public class AddSystemResource {
             AddSystemCommandDTO dto,
             @Context SecurityContext securityContext
     ) throws CommandProcessingException, NoSuchSystemException {
-        SR_Person author = getAuthor(securityContext);
+        SR_Person author = authorMapper.fromSecurityContext(securityContext);
         AddSystemCommand command = dto.toCommand(author, Instant.now());
 
         var addedSystemAndMetadata = handler.handle(command);
 
         return UpdatedSystemDTO.from(addedSystemAndMetadata.getItem1(), addedSystemAndMetadata.getItem2());
-    }
-
-    private SR_Person getAuthor(SecurityContext securityContext) {
-        JsonWebToken jwt = (JsonWebToken) securityContext.getUserPrincipal();
-        return DtoMapper.extractAuthor(jwt);
     }
 }
