8f9dadc2c0e7457aafae87678c76f5b777caae72
Mike Winter <mwinter@thoughtworks.com>
2021-02-23 11:39:09 +0000

    SR-319: Send add system request to API

--- frontend/src/components/AddSystem/AddSystem.js
+++ frontend/src/components/AddSystem/AddSystem.js
@@ -3,11 +3,20 @@ import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 
 import AddSystemForm from "./AddSystemForm";
+import api from "../../services/api";
 
 import "./AddSystem.css";
 
 export default function AddSystem({ onAdd, validateNewSystem }) {
   const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data) => {
+      await api.addSystem(data);
+      onAdd();
+    },
+    [onAdd]
+  );
 
   const handleCancel = useCallback(() => {
     history.push("/");
@@ -15,7 +24,7 @@ export default function AddSystem({ onAdd, validateNewSystem }) {
 
   return (
     <AddSystemForm
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       onCancel={handleCancel}
       validate={validateNewSystem}
     />
--- frontend/src/components/AddSystem/__test__/AddSystem.test.js
+++ frontend/src/components/AddSystem/__test__/AddSystem.test.js
@@ -1,10 +1,10 @@
 import React from "react";
-
-import { render, screen } from "@testing-library/react";
-import { createMemoryHistory } from "history";
-import { Route, Router } from "react-router-dom";
+import user from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter, Route } from "react-router-dom";
 
 import AddSystem from "../AddSystem";
+import api from "../../../services/api";
 
 jest.mock("../../../services/api", () => ({
   addSystem: jest.fn(),
@@ -18,17 +18,48 @@ describe("<AddSystem />", () => {
   });
 
   it("renders add system form", async () => {
-    const history = createMemoryHistory({ initialEntries: ["/add-system"] });
     render(
-      <Router history={history}>
-        <Route path="/add-system">
-          <AddSystem validateNewSystem={() => undefined} onAdd={addHandler} />
-        </Route>
-      </Router>
+      withRouting(
+        <AddSystem validateNewSystem={() => undefined} onAdd={addHandler} />
+      )
     );
 
     const element = await screen.findByText("Add a system to the register");
 
     expect(element).toBeInTheDocument();
   });
+
+  it("sends new system data to the API and refreshes register data on submission", async () => {
+    render(
+      withRouting(
+        <AddSystem validateNewSystem={() => undefined} onAdd={addHandler} />
+      )
+    );
+    const systemNameField = await screen.findByLabelText(/system name/i);
+    const saveButton = screen.getByRole("button", { name: /save/i });
+
+    overtype(systemNameField, "new system");
+    user.click(saveButton);
+
+    await waitFor(() => {
+      expect(api.addSystem).toBeCalledWith({
+        name: "new system",
+      });
+      expect(addHandler).toBeCalled();
+    });
+  });
+
+  function overtype(field, value) {
+    user.clear(field);
+    // noinspection JSIgnoredPromiseFromCall
+    user.type(field, value);
+  }
+
+  function withRouting(component) {
+    return (
+      <MemoryRouter initialEntries={["/add-system"]}>
+        <Route path="/add-system">{component}</Route>
+      </MemoryRouter>
+    );
+  }
 });
