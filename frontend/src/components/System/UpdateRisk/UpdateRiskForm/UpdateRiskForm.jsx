61c69141fc3f3af9ed668e421b406bec4a47cf45
Mike Winter <mwinter@thoughtworks.com>
2021-03-05 16:57:13 +0000

    SR-313: Add rating radio list for updating risks

--- frontend/src/components/System/UpdateRisk/UpdateRiskForm/UpdateRiskForm.jsx
+++ frontend/src/components/System/UpdateRisk/UpdateRiskForm/UpdateRiskForm.jsx
@@ -1,22 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Form, Formik } from "formik";
+import { defaultTo, map } from "lodash-es";
 
+import Radio from "../../../Radio";
 import SecondaryButton from "../../../SecondaryButton";
 import toLower from "../../../../utilities/toLower";
 
+const detailsOf = (risk) => ({
+  level: defaultTo(risk.level, "unknown"),
+});
+const makeRating = (value, text) => ({ value, text });
+const toRadioButton = ({ value, text }, index) => (
+  <Radio key={index} name="level" value={value}>
+    {text}
+  </Radio>
+);
+
+const riskRatings = [
+  makeRating("low", "Low"),
+  makeRating("medium", "Medium"),
+  makeRating("high", "High"),
+  makeRating("unknown", "Unknown"),
+  makeRating("not_applicable", "Not applicable"),
+];
+
 function UpdateRiskForm({ risk, systemName, onCancel }) {
   return (
-    <>
-      <h1>{systemName}</h1>
-      <p className="update-risk-form__secondary">
-        Please provide a high level assessment and enter a rationale for{" "}
-        {toLower(risk.name)} risks associated with your system.
-      </p>
+    <Formik initialValues={detailsOf(risk)} onSubmit={() => {}}>
+      <Form>
+        <h1>{systemName}</h1>
+        <p className="update-risk-form__secondary">
+          Please provide a high level assessment and enter a rationale for{" "}
+          {toLower(risk.name)} risks associated with your system.
+        </p>
 
-      <div className="update-risk-form--risk-controls">
-        <SecondaryButton onClick={onCancel}>Cancel</SecondaryButton>
-      </div>
-    </>
+        <h2 className="update-risk-form__group-title">Roadmap risk rating</h2>
+        <p className="update-risk-form__secondary">
+          What is the level of risk?
+        </p>
+        {map(riskRatings, toRadioButton)}
+
+        <div className="update-risk-form__risk-controls">
+          <SecondaryButton onClick={onCancel}>Cancel</SecondaryButton>
+        </div>
+      </Form>
+    </Formik>
   );
 }
 
--- frontend/src/components/System/UpdateRisk/UpdateRiskForm/UpdateRiskForm.test.jsx
+++ frontend/src/components/System/UpdateRisk/UpdateRiskForm/UpdateRiskForm.test.jsx
@@ -17,6 +17,48 @@ describe("UpdateRiskForm", () => {
     ).toBeVisible();
   });
 
+  describe("risk rating", () => {
+    it("shows the available ratings", () => {
+      const options = [
+        ["low", "Low"],
+        ["medium", "Medium"],
+        ["high", "High"],
+        ["unknown", "Unknown"],
+        ["not_applicable", "Not applicable"],
+      ];
+
+      setUp({ name: "lens_name", rationale: "" });
+
+      const ratingOptions = screen.getAllByRole("radio");
+      expect(ratingOptions).toHaveLength(5);
+      ratingOptions.forEach((option, index) => {
+        const [value, text] = options[index];
+        expect(option.closest("label")).toHaveTextContent(
+          new RegExp(`^${text}$`)
+        );
+        expect(option.value).toBe(value);
+      });
+    });
+
+    it.each(["low", "medium", "high", "unknown", "not_applicable"])(
+      "preselects the current rating: %p",
+      (level) => {
+        setUp({ name: "name", level, rationale: "" });
+
+        expect(screen.getByDisplayValue(level)).toBeChecked();
+      }
+    );
+
+    it.each([null, undefined])(
+      "defaults missing values to unknown: %p",
+      (level) => {
+        setUp({ name: "name", level, rationale: "" });
+
+        expect(screen.getByDisplayValue("unknown")).toBeChecked();
+      }
+    );
+  });
+
   function setUp(risk, systemName = "system") {
     return render(
       <UpdateRiskForm
