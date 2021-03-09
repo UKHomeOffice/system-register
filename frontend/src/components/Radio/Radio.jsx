75d130cb4084adcee9fa2eaabac7f15971b8da6e
Mike Winter <mwinter@thoughtworks.com>
2021-03-09 21:37:58 +0000

    SR-313: Add prop-types to Radio component

--- frontend/src/components/Radio/Radio.jsx
+++ frontend/src/components/Radio/Radio.jsx
@@ -1,23 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Field } from "formik";
 import { Radio as GdsRadio } from "govuk-react";
 
 function Radio({ children, name, value, hint }) {
-  return <Field name={name}>
-    {({ field: { value: formikValue, ...fieldProps } }) => {
-      const inputProps = {
-        value,
-        checked: value === formikValue,
-        ...fieldProps
-      };
+  return (
+    <Field name={name}>
+      {({ field: { value: formikValue, ...fieldProps } }) => {
+        const inputProps = {
+          value,
+          checked: value === formikValue,
+          ...fieldProps,
+        };
 
-      return (
-        <GdsRadio name={name} hint={hint} {...inputProps}>
-          {children}
-        </GdsRadio>
-      );
-    }}
-  </Field>;
+        return (
+          <GdsRadio name={name} hint={hint} {...inputProps}>
+            {children}
+          </GdsRadio>
+        );
+      }}
+    </Field>
+  );
 }
+
+Radio.propTypes = {
+  children: PropTypes.node.isRequired,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  hint: PropTypes.node,
+};
 
 export default Radio;
