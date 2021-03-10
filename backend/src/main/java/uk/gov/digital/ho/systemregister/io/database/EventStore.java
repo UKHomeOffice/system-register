9f9b187c43da06ef91c4f9d18f1341aa3a1dcc09
Mike Winter <mwinter@thoughtworks.com>
2021-03-10 22:23:11 +0000

    Mark event parameter as required in EventStore interface

--- backend/src/main/java/uk/gov/digital/ho/systemregister/io/database/EventStore.java
+++ backend/src/main/java/uk/gov/digital/ho/systemregister/io/database/EventStore.java
@@ -1,5 +1,6 @@
 package uk.gov.digital.ho.systemregister.io.database;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import uk.gov.digital.ho.systemregister.application.messaging.events.SR_Event;
 
 import java.time.Instant;
@@ -8,11 +9,11 @@ import java.util.List;
 import static java.time.Instant.EPOCH;
 
 public interface EventStore {
-    void save(SR_Event event) throws EventStoreException;
+    void save(@NonNull SR_Event event) throws EventStoreException;
 
     default List<SR_Event> getEvents() throws EventStoreException {
         return getEvents(EPOCH);
     }
 
-    List<SR_Event> getEvents(Instant after) throws EventStoreException;
+    List<SR_Event> getEvents(@NonNull Instant after) throws EventStoreException;
 }
